91c946e46620e5191120383d0baacb7ef5f1ee1b
Gustavo Pierre <gustavopierre@gmail.com>
2022-06-10 15:44:43 +0100

    AtmMachine Delete and Update

--- src/main/java/com/zinkwork/Atm/service/AtmMachineService.java
+++ src/main/java/com/zinkwork/Atm/service/AtmMachineService.java
@@ -15,19 +15,36 @@ public class AtmMachineService {
 
 	@Autowired
 	private AtmMachineRepository repository;
-	
-	public List<AtmMachine> findAll(){
+
+	public List<AtmMachine> findAll() {
 		return repository.findAll();
 	}
-	
-	public AtmMachine findById(Long id){
+
+	public AtmMachine findById(Long id) {
 		Optional<AtmMachine> obj = repository.findById(id);
 		return obj.get();
 	}
-	
+
 	public AtmMachine insert(AtmMachine obj) {
 		return repository.save(obj);
 	}
-	
 
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+	
+	public AtmMachine update(Long id, AtmMachine obj) {
+		AtmMachine entity = repository.getOne(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(AtmMachine entity, AtmMachine obj) {
+		entity.setQuantityNote50(obj.getQuantityNote50());
+		entity.setQuantityNote20(obj.getQuantityNote20());
+		entity.setQuantityNote10(obj.getQuantityNote10());
+		entity.setQuantityNote5(obj.getQuantityNote5());
+	}
+
+	
 }
