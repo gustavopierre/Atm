53f4ca6648782fe9babead64438b83556ab5e48b
Gustavo Pierre <gustavopierre@gmail.com>
2022-06-12 01:15:19 +0100

    Exception handling - delete

--- src/main/java/com/zinkwork/Atm/service/AtmService.java
+++ src/main/java/com/zinkwork/Atm/service/AtmService.java
@@ -4,8 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import com.zinkwork.Atm.exception.DatabaseException;
 import com.zinkwork.Atm.exception.ResourceNotFoundException;
 import com.zinkwork.Atm.model.Account;
 import com.zinkwork.Atm.model.AtmMachine;
@@ -37,7 +40,16 @@ public class AtmService {
 	}
 	
 	public void deleteAccount(Long id) {
-		accountRepository.deleteById(id);
+		try {
+			accountRepository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e){
+			throw new ResourceNotFoundException(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	public Account updateAccount(Long id, Account obj) {
